f2f5f701d89564e99f897119501d3a96798f95a3
anastasia.maidacenco <maidacenco100@gmail.com>
2023-02-26 21:10:05 +0200

    Create project Add dependency Configure application.properties

---
+++ src/main/java/com/example/laborator_crud/LaboratorCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.laborator_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratorCrudApplication.class, args);
+    }
+
+}
