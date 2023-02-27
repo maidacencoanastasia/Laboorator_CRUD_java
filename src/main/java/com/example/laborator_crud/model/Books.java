8d5aae1389339c004afb7a5d97de616c3e5abc6c
anastasia.maidacenco <maidacenco100@gmail.com>
2023-02-27 16:40:31 +0200

    Create model Books

---
+++ src/main/java/com/example/laborator_crud/model/Books.java
@@ -0,0 +1,28 @@
+package com.example.laborator_crud.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "books")
+public class Books {
+    @Id
+    @Column(name = "isbn", nullable = false)
+    private String isbn;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "author_id", nullable = false)
+    private int author_id;
+
+    @Column(name = "publisher_id", nullable = true)
+    private int publisher_id;
+
+    @Column(name = "year_pub", nullable = true)
+    private String year_pub;
+
+    @Column(name = "description", nullable = true)
+    private String description;
+}
