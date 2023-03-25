f0919fda7a299e8e913ee1df33869793041eac94
anastasia.maidacenco <maidacenco100@gmail.com>
2023-03-25 13:10:14 +0200

    change to javax from jakarta add swagger-ui clean up code

--- src/main/java/com/example/backend/model/Authors.java
+++ src/main/java/com/example/backend/model/Authors.java
@@ -1,7 +1,9 @@
 package com.example.backend.model;
 
 //import jakarta.persistence.*;
+
 import lombok.Data;
+
 import javax.persistence.*;
 import javax.persistence.Entity;
 import javax.persistence.Table;
